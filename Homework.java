cd648d3794e2e6d228a10921b5ad61ef718b2af7
reaper <reaper>
2023-02-23 09:11:56 +0300

    Добавление пирамидальной сортировки

--- Homework.java
+++ Homework.java
@@ -11,6 +11,20 @@ public class Homework {
     
     //функция для выполнения пирамидальной сортировки массива `A` размера `n`
     public static void heapSort(int[] sortArr) {
-        
+    //строим приоритетную очередь и инициализируем ее заданным массивом
+    int n = sortArr.length;
+
+    //build-heap: вызывать heapify, начиная с последнего внутреннего
+    //узел до корневого узла
+    int i = (n - 2) / 2;
+    while (i >= 0) {
+        heapify(sortArr, i--, n);
+    }
+
+    //несколько раз извлекаем из кучи, пока она не станет пустой
+    while (n > 0) {
+        sortArr[n - 1] = pop(sortArr, n);
+        n--;
+    }
     }
 }
