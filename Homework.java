1d64601c0cdffc925951f742eec9225a9ec37f81
reaper <reaper>
2023-02-23 09:08:18 +0300

    Начало программы

--- Homework.java
+++ Homework.java
@@ -1,17 +1,16 @@
 import java.util.Arrays;
 
 public class Homework {
-    
-}
-public static void main(String args[]) {
-    int[] sortArr = {12, 6, 4, 1, 15, 10};
-    heapSort(sortArr);
-    for(int i = 0; i < sortArr.length; i++){
-        System.out.print(sortArr[i] + "\n");
+    public static void main(String args[]) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        heapSort(sortArr);
+        for(int i = 0; i < sortArr.length; i++){
+            System.out.print(sortArr[i] + "\n");
+        }
     }
-}
-
-//функция для выполнения пирамидальной сортировки массива `A` размера `n`
-public static void heapSort(int[] sortArr) {
     
+    //функция для выполнения пирамидальной сортировки массива `A` размера `n`
+    public static void heapSort(int[] sortArr) {
+        
+    }
 }
